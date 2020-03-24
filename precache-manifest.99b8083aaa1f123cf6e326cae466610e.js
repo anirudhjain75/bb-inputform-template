5cb3ff37df25577bbb819a95421ecec34293abbf
Anirudh Jain <anirudh.jain@outlook.com>
2020-03-24 15:31:41 +0530

    Updates

---
+++ precache-manifest.99b8083aaa1f123cf6e326cae466610e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecfed9f5264127f904b42fc354cd6ba",
+    "url": "/bb-inputform-template/index.html"
+  },
+  {
+    "revision": "b9fb852754bf448403b9",
+    "url": "/bb-inputform-template/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "50a8b6618cd73a777fe1",
+    "url": "/bb-inputform-template/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9fb852754bf448403b9",
+    "url": "/bb-inputform-template/static/js/2.dcae3faa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bb-inputform-template/static/js/2.dcae3faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a8b6618cd73a777fe1",
+    "url": "/bb-inputform-template/static/js/main.1e245210.chunk.js"
+  },
+  {
+    "revision": "48653201023b568e402e",
+    "url": "/bb-inputform-template/static/js/runtime-main.4d2d3d9c.js"
+  }
+]);
